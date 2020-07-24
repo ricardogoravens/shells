850ea0324c39cf9d2dd14faa5a68cb4aaccc6682
lnxricardo <solucionartec@gmail.com>
2020-07-24 11:24:46 -0300

    finalizando

--- zabbix/read.sh
+++ zabbix/read.sh
@@ -1,20 +1,26 @@
-zbxconf="/etc/zabbix/zabbix_agent.conf"
-zbxbak="/etc/zabbix/zabbix_agent.conf.bak"
+zbxconf="/etc/zabbix/zabbix_agentd.conf"
+zbxbak="/etc/zabbix/zabbix_agentd.conf.bak"
 include="Include=/etc/zabbix/scripts/*conf"
 file="security.conf"
 filebak="security.conf.bak"
-dir="/etc/zabbix/scripts"
+dir="/etc/zabbix/scripts/"
 today=`date +"%d-%m-%y-%H:%M:%S"`
+update="#script atualizado em "
+server1="Server=100.24.155.31"
+server2="ServerActive=100.24.155.31"
 
-if [[ !-d "$dir" ]]; then
+if [ ! -e "$dir" ]; then
         mkdir -p "$dir"
         cp $file $dir
-        cp $zbxconf $zbxbak
+        cp "$zbxconf" "$zbxbak-$today"
         echo $include >> $zbxconf
-
+        echo $update $today >> $zbxconf
+        
+    
         /etc/init.d/zabbix-agent restart
 else
         mv "$dir"/"$file" "$dir"/"$filebak-$today"
-        cp $file $dir
+        cp $file $dir 
+        echo $update $today >> $zbxconf
         /etc/init.d/zabbix-agent restart
 fi
