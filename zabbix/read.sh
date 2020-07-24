55a2edb1853c617405da0f9a1aec9bc20c5354d1
lnxricardo <solucionartec@gmail.com>
2020-07-24 10:54:19 -0300

    correcao3

--- zabbix/read.sh
+++ zabbix/read.sh
@@ -6,7 +6,7 @@ filebak="security.conf.bak"
 dir="/etc/zabbix/scripts"
 today=`date +"%d-%m-%y-%H:%M:%S"`
 
-if [[ -d "$dir" ]]; then
+if [[ !-d "$dir" ]]; then
         mkdir -p "$dir"
         cp $file $dir
         cp $zbxconf $zbxbak
