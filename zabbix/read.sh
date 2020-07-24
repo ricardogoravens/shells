908ab182b39373f2371269891d338381afd821fd
lnxricardo <solucionartec@gmail.com>
2020-07-24 10:32:37 -0300

    primeiro commit

---
+++ zabbix/read.sh
@@ -0,0 +1,19 @@
+zbxconf="/etc/zabbix/zabbix_agent.conf"
+zbxbak="/etc/zabbix/zabbix_agent.conf.bak"
+include="Include=/etc/zabbix/scripts/*conf"
+file="security.conf"
+filebak="security.conf.bak"
+dir="/etc/zabbix/scripts"
+
+if [[ -d "$dir" ]]; then
+        mkdir -p "$dir"
+        cp $file $dir
+        cp $zbxconf $zbxbak
+        echo $include >> $zbxconf
+
+        /etc/init.d/zabbix_agent restart
+else
+        mv "$dir"/"$file" "$dir"/$filebak.date +"%d-%m-%y-%H:%M:%S"
+        cp $file $dir
+        /etc/init.d/zabbix_agent restart
+fi
